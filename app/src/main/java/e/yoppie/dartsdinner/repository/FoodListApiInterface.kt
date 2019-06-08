8dd73de32b824ebbb24604b0c3f583da3af7b041
yoshiya12x <hikowaiide@gmail.com>
2019-06-08 17:26:50 +0900

    change endpoint

--- app/src/main/java/e/yoppie/dartsdinner/repository/FoodListApiInterface.kt
+++ app/src/main/java/e/yoppie/dartsdinner/repository/FoodListApiInterface.kt
@@ -5,7 +5,7 @@ import io.reactivex.Observable
 import retrofit2.http.GET
 
 interface FoodListApiInterface {
-    @GET("stage1/dinnerlisttest")
+    @GET("stage1/dinnerlist")
     fun getFoodList(
     ): Observable<FoodList>
 }
